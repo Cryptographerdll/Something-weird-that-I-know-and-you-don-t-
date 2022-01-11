be476cb81eb8370eb9752f535f00bb572173e020
Cryptographer <85350190+CryptographyHacker@users.noreply.github.com>
2022-01-11 18:12:12 +0100

    Add files via upload

---
+++ DemoCode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handling_Exceptions_Master
+{
+    class DemoCode
+    {
+        public int GrandParentMehod(int position)
+        {
+            int output = 0;
+            Console.WriteLine("open database connection ");
+
+            try
+            {
+                output = ParentMethod(position);
+            }
+
+            catch (Exception ex)
+            {
+                // Do some Logging
+                throw; //  new ArgumentException("You past into bad data", ex);
+            }
+
+            finally
+            {
+                Console.WriteLine("close the database connections ");
+            }
+
+            return output;
+        }
+
+        public int ParentMethod(int position)
+        {
+            return GetNumber(position);
+        }
+
+        public int GetNumber(int position)
+        {
+            int output = 0;
+
+            int[] Numbers = new int[] { 1, 4, 2, 11, 78, 14, 92, 85, 6, 12, 4 };
+            output = Numbers[position];
+            //try
+            //{
+            //    int[] Numbers = new int[] { 1, 4, 2, 11, 78, 14, 92, 85, 6, 12, 4 };
+            //    output = Numbers[position];
+            //}
+
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            return output;
+
+            #region condition <= 10 if ! well throw an exception .
+            #endregion
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ExceptionsLibrary;
+
+namespace Handling_Exceptions_Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+
+            try
+            {
+                int result = demo.GrandParentMehod(8);
+                Console.WriteLine($"the value of given position is {result}");
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("You enter a bad value you are a bad user !!");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+                var inner = ex.InnerException;
+
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
